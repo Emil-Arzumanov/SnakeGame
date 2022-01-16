fb090445417841b4714fb772c5cbb6255e4f8da2
Emil-Arzumanov <71209435+Emil-Arzumanov@users.noreply.github.com>
2022-01-16 21:14:37 +0500

    Figured out main func

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
-let previousRenderTime;
-const SNAKE_SPEED = 2;
+let previousTime = 0;
+const SNAKE_SPEED = 1;
 
 function main(currentTime) {
+    let currentRenderTime = (currentTime - previousTime)/1000;
     window.requestAnimationFrame(main);
+    if (currentRenderTime < 1/SNAKE_SPEED) return;
     console.log(currentTime);
+    previousTime = currentTime;
 }
 
 window.requestAnimationFrame(main)
