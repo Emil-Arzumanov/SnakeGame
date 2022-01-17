37bd214792b18a1f53e004cd727fcb40029d6d9a
Emil-Arzumanov <71209435+Emil-Arzumanov@users.noreply.github.com>
2022-01-18 02:01:46 +0500

    Snake can die

--- game.js
+++ game.js
@@ -2,8 +2,15 @@ import {SNAKE_SPEED as SNAKE_SPEED, draw as drawSnake, update as updateSnake} fr
 import {draw as drawFood, update as updateFood} from './food.js';
 
 let previousTime = 0;
+let snakeGridRow;
+let snakeGridColumn;
 
 function main(currentTime) {
+    if (snakeGridRow < 1 || snakeGridRow > 21 || snakeGridColumn < 1 || snakeGridColumn > 21) {
+        alert("You lose! :(")
+        document.location.reload();
+        return;
+    }
     let currentRenderTime = (currentTime - previousTime) / 1000;
     window.requestAnimationFrame(main);
     if (currentRenderTime < 1 / SNAKE_SPEED) return;
@@ -17,6 +24,8 @@ function draw() {
     gameBoard.innerHTML = '';
     drawSnake(gameBoard);
     drawFood(gameBoard);
+    snakeGridRow = document.querySelectorAll(".snake")[0].style.gridRowStart;
+    snakeGridColumn = document.querySelectorAll(".snake")[0].style.gridColumnStart;
 };
 
 function update() {
